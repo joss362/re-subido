72e7db79917be91c40bb4ec79d210b6b01738aee
joss362 <velasquej88@gmail.com>
2024-12-11 23:34:59 -0500

    iniciar

--- src/app/auth/features/sign-up/sign-up.component.ts
+++ src/app/auth/features/sign-up/sign-up.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import {ReactiveFormsModule} from from '@angular/forms';
 
+
+
+
+interface FormSignUp {
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
+}
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
-  styles: ``
+  
 })
-export default class SignUpComponent {}
+export default class SignUpComponent {
+  private _formBuilder = inject(FormnBuider);
+
+  form =this._formBuilder.group<any>({
+    email: this._formBuilder.control('',[
+      Validators.required,
+      Validators.email,
+    ]),
+    password: this._formBuilder.control('',Validators.required)
+  });
+}
